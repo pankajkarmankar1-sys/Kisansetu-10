4a9fda9484559b32c8c5b1e1b83acafd85f7643d
pankajkarmankar1-sys <pankajkarmankar1@gmail.com>
2026-06-22 17:47:55 +0530

    Add files via upload

---
+++ _document.jsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html lang="hi">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
